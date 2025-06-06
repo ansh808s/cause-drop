57928f0868740a95afb542a88140e8a8d76cd8f3
bitbard3 <ansharora3839@gmail.com>
2025-06-07 02:25:48 +0530

    Setup next in client app

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "../index.css";
+import Providers from "@/components/providers";
+import Header from "@/components/header";
+
+const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Cause Drop",
+  description: "Create donation campaign and share them as Blinks.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={` ${manrope.variable} antialiased`}>
+        <Providers>
+          <div className="grid grid-rows-[auto_1fr] h-svh">
+            <Header />
+            {children}
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
