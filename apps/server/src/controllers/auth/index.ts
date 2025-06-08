899c507fdb804559935a033208ab4f866ece05d0
bitbard3 <ansharora3839@gmail.com>
2025-06-09 04:51:20 +0530

    feat: Add signin controller

---
+++ apps/server/src/controllers/auth/index.ts
@@ -0,0 +1,68 @@
+import type { RequestHandler } from "express";
+import prisma from "prisma";
+import jwt from "jsonwebtoken";
+import { signinSchema } from "./validation";
+import { verifySignature } from "utils/verifySignature";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+export const signin: RequestHandler = async (req, res) => {
+  const validatedData = signinSchema.safeParse(req.body);
+
+  if (!validatedData.success) {
+    res.status(400).json({
+      success: false,
+      error: "Validation error",
+    });
+    return;
+  }
+  const { publicKey, signature, message } = validatedData.data;
+  const isValidSignature = verifySignature(publicKey, signature, message);
+
+  if (!isValidSignature) {
+    res.status(401).json({
+      success: false,
+      error: "Invalid signature",
+    });
+    return;
+  }
+  try {
+    let user = await prisma.user.findFirst({
+      where: {
+        address: publicKey,
+      },
+    });
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          address: publicKey,
+        },
+      });
+    }
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        publicKey: publicKey,
+      },
+      JWT_SECRET
+    );
+
+    res.status(200).json({
+      success: true,
+      data: {
+        token,
+        user: {
+          id: user.id,
+          createdAt: user.createdAt,
+        },
+      },
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
+    return;
+  }
+};
