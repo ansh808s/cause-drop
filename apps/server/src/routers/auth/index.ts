486e963b0a05c9abff46b833701b77cd52b67c3d
bitbard3 <ansharora3839@gmail.com>
2025-06-09 06:10:31 +0530

    feat: Add signin and verify routes

--- apps/server/src/routers/auth/index.ts
+++ apps/server/src/routers/auth/index.ts
@@ -1,5 +1,9 @@
+import { signin, verify } from "@/controllers/auth";
 import express, { Router } from "express";
 
 const router: Router = express.Router();
+
+router.post("/signin", signin);
+router.post("/verify", verify);
 
 export default router;
