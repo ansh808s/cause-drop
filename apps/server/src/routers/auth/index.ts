ddec53404092ded81798a4ede65623c98dfe073f
bitbard3 <ansharora3839@gmail.com>
2025-06-09 16:08:05 +0530

    feat: Add signedurl route

--- apps/server/src/routers/auth/index.ts
+++ apps/server/src/routers/auth/index.ts
@@ -1,9 +1,10 @@
-import { signin, verify } from "@/controllers/auth";
+import { signedURL, signin, verify } from "@/controllers/auth";
 import express, { Router } from "express";
 
 const router: Router = express.Router();
 
 router.post("/signin", signin);
 router.post("/verify", verify);
+router.post("/signedurl", signedURL);
 
 export default router;
