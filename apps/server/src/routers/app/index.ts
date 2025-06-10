6a638f12c4ef90039fe95d3aa1f93f9d371f579e
bitbard3 <ansharora3839@gmail.com>
2025-06-10 12:12:20 +0530

    feat: Add app routes

--- apps/server/src/routers/app/index.ts
+++ apps/server/src/routers/app/index.ts
@@ -1,5 +1,15 @@
+import {
+  createCampaign,
+  getCampaign,
+  getUserCampaigns,
+} from "@/controllers/app";
+import { authenticateToken } from "@/middlewares";
 import express, { Router } from "express";
 
 const router: Router = express.Router();
+
+router.post("/campaign", authenticateToken, createCampaign);
+router.get("/campaign/:slug", getCampaign);
+router.get("/campaign", authenticateToken, getUserCampaigns);
 
 export default router;
