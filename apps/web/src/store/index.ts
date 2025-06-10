d4c90554aaa961cd47b1a8082106c91b14c094c5
bitbard3 <ansharora3839@gmail.com>
2025-06-10 10:15:37 +0530

    feat: Add redux store configuration

---
+++ apps/web/src/store/index.ts
@@ -0,0 +1,25 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import campaignSlice from "./slices/campaignSlice";
+
+export const store = configureStore({
+  reducer: {
+    campaign: campaignSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
