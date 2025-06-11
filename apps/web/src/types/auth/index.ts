12009009de70ca63fa6d279246e1a36727d242d5
bitbard3 <ansharora3839@gmail.com>
2025-06-11 15:00:58 +0530

    feat: Add request and response type for signin

--- apps/web/src/types/auth/index.ts
+++ apps/web/src/types/auth/index.ts
@@ -2,3 +2,13 @@ export interface User {
   id: string;
   createdAt: string;
 }
+export interface SignInRequest {
+  publicKey: string;
+  signature: string;
+  message: string;
+}
+
+export interface SignInResponseData {
+  token: string;
+  user: User;
+}
