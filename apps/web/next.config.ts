f56951e206347f1429cb2c9d5db76aa2824da52a
bitbard3 <ansharora3839@gmail.com>
2025-06-13 15:25:09 +0530

    feat: Add cloudfront url

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://db9qe88uankx8.cloudfront.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
