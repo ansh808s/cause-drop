8c30df71d542e5563fcc1eaa881b63920964365e
bitbard3 <ansharora3839@gmail.com>
2025-06-17 13:33:26 +0530

    feat: Add donation post controller

--- apps/server/src/controllers/actions/index.ts
+++ apps/server/src/controllers/actions/index.ts
@@ -18,6 +18,7 @@ import {
 import { getCampaignSchema } from "../app/validation";
 
 const headers = createActionHeaders();
+const connection = new Connection(clusterApiUrl("devnet"));
 
 export const getCampaignAction: RequestHandler = async (req, res) => {
   try {
@@ -45,7 +46,7 @@ export const getCampaignAction: RequestHandler = async (req, res) => {
     }
 
     const recipient = new PublicKey(campaign.recipient);
-    const baseHref = `http://localhost:3000/api/actions/donation?to=${recipient.toBase58()}`;
+    const baseHref = `https://api.causedrop.ansharora.me/api/actions/donation?to=${recipient.toBase58()}`;
 
     const payload: ActionGetResponse = {
       type: "action",
@@ -56,17 +57,17 @@ export const getCampaignAction: RequestHandler = async (req, res) => {
       links: {
         actions: [
           {
-            label: "Send 1 SOL",
+            label: "Send 0.1 SOL",
             href: `${baseHref}&amount=0.1`,
             type: "post",
           },
           {
-            label: "Send 5 SOL",
+            label: "Send 0.25 SOL",
             href: `${baseHref}&amount=0.25`,
             type: "post",
           },
           {
-            label: "Send 10 SOL",
+            label: "Send 0.5 SOL",
             href: `${baseHref}&amount=0.5`,
             type: "post",
           },
@@ -98,4 +99,60 @@ export const getCampaignAction: RequestHandler = async (req, res) => {
 
 export const donationOption: RequestHandler = (req, res) => {
   res.set(headers).json(null);
+};
+
+export const donation: RequestHandler = async (req, res) => {
+  try {
+    const { amount, to } = req.query;
+    const { account } = req.body;
+    const toPubkey = new PublicKey(to as string);
+    if (!amount) {
+      throw new Error("Please enter an amount");
+    }
+    if (parseFloat(amount as string) <= 0) {
+      throw new Error("Amount should be greater than 0");
+    }
+    if (!account) {
+      throw new Error('Invalid "account" provided');
+    }
+    const fromPubkey = new PublicKey(account);
+    const minimumBalance = await connection.getMinimumBalanceForRentExemption(
+      0
+    );
+
+    if (parseFloat(amount as string) * LAMPORTS_PER_SOL < minimumBalance) {
+      throw new Error(`Account may not be rent exempt: ${toPubkey.toBase58()}`);
+    }
+
+    const transferSolInstruction = SystemProgram.transfer({
+      fromPubkey: fromPubkey,
+      toPubkey: toPubkey,
+      lamports: parseFloat(amount as string) * LAMPORTS_PER_SOL,
+    });
+
+    const { blockhash, lastValidBlockHeight } =
+      await connection.getLatestBlockhash();
+
+    const transaction = new Transaction({
+      feePayer: fromPubkey,
+      blockhash,
+      lastValidBlockHeight,
+    }).add(transferSolInstruction);
+
+    const payload = await createPostResponse({
+      fields: {
+        type: "transaction",
+        transaction,
+        message: `Send ${amount} SOL to ${toPubkey.toBase58()}`,
+      },
+    });
+
+    res.json(payload);
+    return;
+  } catch (error) {
+    res
+      .status(400)
+      // @ts-ignore
+      .json({ error: error.message || "An unknown error occurred" });
+  }
 };
