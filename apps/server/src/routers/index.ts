814354ca147e50ade9cf7bdaa0bad0cee8fdd8a5
bitbard3 <ansharora3839@gmail.com>
2025-06-17 12:59:17 +0530

    feat: Create action router

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,7 +1,10 @@
 import express, { Router } from "express";
 import authRouter from "./auth";
 import appRouter from "./app";
+import actionsRouter from "./actions";
+
 export const router: Router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/app", appRouter);
+router.use("/actions", actionsRouter);
