9893491d58f541afadaf12523cb84092f7760493
bitbard3 <ansharora3839@gmail.com>
2025-06-17 13:33:39 +0530

    feat: Add donation post route

--- apps/server/src/routers/actions/index.ts
+++ apps/server/src/routers/actions/index.ts
@@ -1,9 +1,14 @@
-import { donationOption, getCampaignAction } from "@/controllers/actions";
+import {
+  donation,
+  donationOption,
+  getCampaignAction,
+} from "@/controllers/actions";
 import express, { Router } from "express";
 
 const router: Router = express.Router();
 
 router.get("/donation/:slug", getCampaignAction);
 router.options("/donation", donationOption);
+router.post("/donation", donation);
 
 export default router;
